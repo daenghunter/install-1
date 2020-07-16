62a206c9d3910487f8f1f7a4ea7fa0909f4747d1
daenghunter <65704433+daenghunter@users.noreply.github.com>
2020-07-16 22:12:40 +0800

    cek.sh

--- cek.sh
+++ cek.sh
@@ -2,9 +2,9 @@
 
 data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
 
-echo "-----------------------";
+echo "••••••••••••••••••••••••";
 echo "Checking Dropbear login";
-echo "-----------------------";
+echo "••••••••••••••••••••••••";
 
 for PID in "${data[@]}"
 do
@@ -21,9 +21,9 @@ echo "";
 
 data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
 
-echo "----------------------";
+echo "••••••••••••••••••••••••";
 echo "Checking OpenSSH login";
-echo "----------------------";
+echo "••••••••••••••••••••••••";
 
 for PID in "${data[@]}"
 do
@@ -38,10 +38,10 @@ done
 
 echo "";
 
-echo "------------------------------------------------"
+echo "••••••••••••••••••••••••"
 echo " Multi Login = kill "
 echo " Cara pakai : kill [nombor PID] "
-echo "------------------------------------------------"
+echo "••••••••••••••••••••••••"
 
-echo " By Admin VPNSTORES"
+echo " © Daeng Hunter Groups"
 echo "";
