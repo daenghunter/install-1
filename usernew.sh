dade20a6150666d8b00d6789ee71d5c6bab077b5
daenghunter <65704433+daenghunter@users.noreply.github.com>
2020-07-16 22:40:18 +0800

    usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -5,7 +5,7 @@ read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
 
 IP=`curl icanhazip.com`
-echo Script AutoCreate Akun SSH dan OpenVPN by Horas
+echo Script Auto Create SSH & OpenVPN
 sleep 1
 echo Ping Host
 echo Cek Hak Akses...
@@ -22,8 +22,9 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "Informasi SSH"
-echo -e "=========-account-=========="
+echo -e "Terimakasih telah berlangganan di Daeng SSH Server"
+echo -e "Berikut detail account yang telah kami buat"
+echo -e "•••••••••••••••••••••••••••••"
 echo -e "Host		: $IP" 
 echo -e "OpenSSH	: 22,"
 echo -e "Dropbear	: 109, 110, 143, 456"
@@ -34,7 +35,7 @@ echo -e "OpenVPN  	: UDP 2200 (client config : http://$IP:81/client-udp-2200.ovp
 echo -e "badvpn		: 7200/7300"
 echo -e "Username	: $Login "
 echo -e "Password	: $Pass"
-echo -e "-----------------------------"
+echo -e "•••••••••••••••••••••••••••••"
 echo -e "Aktif Sampai	: $exp"
-echo -e "==========================="
-echo -e "Script by Admin VPNSTORES"
+echo -e "•••••••••••••••••••••••••••••"
+echo -e "© Daeng Hunter Groups"
