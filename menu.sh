578caa9cc38fa0816a77499209f0a81345fa5369
daenghunter <65704433+daenghunter@users.noreply.github.com>
2020-07-27 00:52:33 +0800

    menu.sh

--- menu.sh
+++ menu.sh
@@ -2,7 +2,7 @@
 #Menu
 
 echo -e "Daftar Perintah Fitur Daeng SSH Server!"
-echo -e "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+echo -e "•••••••••••••••••••••••••••••••••••••••••••••••••••••••"
 echo -e "* menu      : menampilkan daftar perintah"
 echo -e "* usernew   : membuat akun SSH & OpenVPN"
 echo -e "* trial     : membuat akun trial"
@@ -18,6 +18,6 @@ echo -e "* info      : menampilkan informasi sistem"
 echo -e "* about     : info script auto install"
 echo -e "* exit      : keluar dari Putty/Connecbot/"
 echo -e "              JuiceSSH"
-echo -e "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+echo -e "•••••••••••••••••••••••••••••••••••••••••••••••••••••••"
 echo -e "Script © Daeng Hunter Groups"
 echo -e ""
